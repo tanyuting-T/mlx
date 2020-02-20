3a66f4bb2b666d70c78643197a29be28999d963b
tanyuting-T <54108210+tanyuting-T@users.noreply.github.com>
2020-02-20 20:11:01 +0800

    Update psi_new.py

--- psi_new.py
+++ psi_new.py
@@ -5,26 +5,28 @@ np.set_printoptions(precision=5,suppress=True)
 
 class PSI:
     def __init__(self,**opts): #通过赋值的方式以字典的形式传入参数
-        self.num_orbital=None  # m
-        self.num_particle=None  # N
+        self.num_orbital=0  # m
+        self.num_particle=0  # N
         
         self.num_numberstate=opts.get("n",None)  # n
         
-        self.num_subparticle_x=None  # mx
-        self.num_subparticle_y=None  # my
-        self.num_primitive_x=None  # nx
-        self.num_primitive_y=None  # ny
-        
-        self.psi_topnode=np.zeros(self.num_numberstate,dtype=complex)
-        self.psi_numberstate=np.zeros((self.num_numberstate,self.num_orbital),dtype=int)
-        self.psi_particle=np.zeros((self.num_orbital,self.num_subparticle_x,self.num_subparticle_y),dtype=complex)
-        self.psi_subparticle_x=np.zeros((self.num_subparticle_x,self.num_primitive_x),dtype=complex)
-        self.psi_subparticle_y=np.zeros((self.num_subparticle_y,self.num_primitive_y),dtype=complex)
+        self.num_subparticle_x=0  # mx
+        self.num_subparticle_y=0  # my
+        self.num_primitive_x=0  # nx
+        self.num_primitive_y=0  # ny
         
         # start from tape
         tape = opts.get("tape",None)
         if tape is not None:
             self._init_from_tape(tape)
+        
+        self.num_numberstate=maths.num_numberstate(self.num_particle,self.num_orbital)
+        self.psi_numberstate=maths.state_arrange(self.num_numberstate,self.num_orbital)
+        
+        self.psi_topnode=np.zeros(self.num_numberstate,dtype=complex)
+        self.psi_particle=np.zeros((self.num_orbital,self.num_subparticle_x,self.num_subparticle_y),dtype=complex)
+        self.psi_subparticle_x=np.zeros((self.num_subparticle_x,self.num_primitive_x),dtype=complex)
+        self.psi_subparticle_y=np.zeros((self.num_subparticle_y,self.num_primitive_y),dtype=complex)
         
         
     def _init_from_tape(self,tape):
@@ -60,23 +62,29 @@ class PSI:
         self.num_orbital = pnode[1]
         self.num_subparticle_x = subnode[0]
         self.num_subparticle_y = subnode[1]
+        self.num_primitive_x= subnode_s[0]
+        self.num_primitive_y= subnode_s[1]
         
-    def PSI_ini(self,ns,startSPF,spfx,spfy):
+    def PSI_ini(self,spf_ns,spf_particle,spfx,spfy):
         """
-        initializes coefficients of initial wave function of a bosonic layer.
+        initializes coefficients of initial wavefunction.
         """
-        #无截断的情况
-        self.num_numberstate = maths.num_numberstate(self.num_particle,self.num_orbital)
+        if self.psi_topnode.shape==spf_ns.shape:
+            self.psi_topnode=spf_ns
+        else:            
+            raise ValueError('Input does not match the dimension of topnode spf')
         
-        #psi赋值（未完成）
-        self.psi_topnode=np.zeros(self.num_numberstate,dtype=complex)
-        self.psi_numberstate=np.zeros((self.num_numberstate,self.num_orbital),dtype=int)
+        if self.psi_particle.shape==spf_particle.shape:
+            self.psi_particle=spf_particle
+        else:            
+            raise ValueError('Input does not match the dimension of particle spf')
         
-        self.psi_particle=startSPF
-        self.psi_subparticle_x=spfx
-        self.psi_subparticle_y=spfy
+        if self.psi_subparticle_x.shape==spfx.shape:
+            self.psi_subparticle_x=spfx
+        else:            
+            raise ValueError('Input does not match the dimension of subparticle_x spf')
         
-        
-        
-        return psi_ini
-    
+        if self.psi_subparticle_y.shape==spfy.shape:
+            self.psi_subparticle_y=spfy
+        else:            
+            raise ValueError('Input does not match the dimension of subparticle_y spf')
